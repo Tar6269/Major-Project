60cb4f9c27bfb31df02553c2315f0c779a8ca0a2
Tar6269 <114016175+Tar6269@users.noreply.github.com>
2022-11-22 00:12:06 -0600

    testing server client websockets behaviour

--- sketch.js
+++ sketch.js
@@ -4,33 +4,41 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let buttonWidth;
+let buttonHeight;
+let hostButtonX;
+let hostButtonY;
+let joinButtonX;
+let joinButtonY;
 let hostOrClient
 let displayTest = false;
+let canvas;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.center("horizontal");
+  buttonWidth = windowWidth/3
+  buttonHeight = windowHeight/5
+  hostButtonX = windowWidth/2 - buttonWidth/2;
+  hostButtonY = windowHeight/2;
+  joinButtonX = windowWidth/2 + buttonWidth/2;
+  joinButtonY = windowHeight/2;
 }
 
 function draw() {
   background(220);
   menuScreen();
   if(displayTest){
-    text("it worked", width/2, height/2);
+
   }
 }
 
 function menuScreen(){
-  let buttonWidth = windowWidth/3
-  let buttonHeight = windowHeight/5
-  let hostButtonX = windowWidth/2 - buttonWidth/2;
-  let hostbuttonY = windowHeight/2;
-  let joinButtonX = windowWidth/2 + buttonWidth/2;
-  let joinButtonY = windowHeight/2;
+
   rectMode(CENTER);
   textAlign(CENTER, CENTER);
   textSize(windowWidth/10)
-  rect(hostButtonX, hostbuttonY, buttonWidth, buttonHeight);
-  text("Host", hostButtonX, hostbuttonY);
+  rect(hostButtonX, hostButtonY, buttonWidth, buttonHeight);
+  text("Host", hostButtonX, hostButtonY);
 
   rect(joinButtonX, joinButtonY, buttonWidth, buttonHeight);
   text("Join", joinButtonX, joinButtonY);
@@ -39,7 +47,15 @@ function menuScreen(){
 
 
 function mouseClicked(){
-  if(mouseX > hostButtonX && mouseX < (hostbuttonX + buttonWidth) && mouseY > hostButtonY && mouseY < (hostButtonY + buttonHeight)){
-    displayTest = true;
+  if(mouseX > (hostButtonX - buttonWidth/2) && mouseX < (hostButtonX + buttonWidth/2) && mouseY > (hostButtonY - buttonHeight/2) && mouseY < (hostButtonY + buttonHeight/2)){
+    connectToHost()
   }
+}
+
+function connectToHost(){
+  const ws = new WebSocket("ws://localhost:8082");
+  ws.addEventListener("open", () =>{
+  console.log("We are connected!")
+});
+
 }
